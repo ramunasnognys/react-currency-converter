9049ed9ae08c65c3564c870f97f41d0941fdcb3d
Ramunas <dev.nognys@gmail.com>
2022-08-28 18:15:45 +0200

    complete app

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 export default function CurrencyRow(props) {
     const {
-        currencyOptions
+        currencyOptions,
+        selectedCurrency,
+        onChangeCurrency,
+        onChangeAmount,
+        amount
     } = props
 
   return (
     <div>
-        <input type="number" />
-        <select>
+        <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
+        <select value={selectedCurrency} onChange={onChangeCurrency}>
             {currencyOptions.map(option => (
                 <option key={option} value={option}>{option}</option>
             ))}
