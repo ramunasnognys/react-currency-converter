c162b91789c4c6016fafe637de83ca31ca4dc88f
Ramunas <dev.nognys@gmail.com>
2022-08-28 17:11:42 +0200

    Imported currencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import CurrencyRow from './CurrencyRow'
 
@@ -6,18 +6,27 @@ const BASE_URL = 'https://api.exchangerate.host/latest'
 
 
 function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+      });
   }, []) 
 
   return (
     <>
       <h1>Convert </h1>
-      <CurrencyRow />
+      <CurrencyRow 
+      currencyOptions={currencyOptions}
+      />
       <div></div>
-      <CurrencyRow />
+      <CurrencyRow 
+      currencyOptions={currencyOptions}
+      />
     </>
   );
 }
